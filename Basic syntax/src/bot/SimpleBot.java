07d357441ab018270854df2eaca9c4a3f89f1041
Hassan <hassanalihazaraa@gmail.com>
2020-10-02 10:57:29 +0200

    print string + name

---
+++ Basic syntax/src/bot/SimpleBot.java
@@ -0,0 +1,10 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        String name = "Aid";
+        int birthDay = 2020;
+        System.out.println("Hello! My name is " + name + "\n" +
+                "I was created in " + birthDay + ".");
+    }
+}
