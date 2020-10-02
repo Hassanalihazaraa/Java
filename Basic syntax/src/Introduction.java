5e1d21ae4cbbba3d8baabd8d78a19c1d4523c3a3
Hassan <hassanalihazaraa@gmail.com>
2020-10-02 10:29:47 +0200

    put all code in a method

--- Basic syntax/src/Introduction.java
+++ Basic syntax/src/Introduction.java
@@ -1,6 +1,10 @@
 public class Introduction {
-    //printing data
     public static void main(String[] args) {
+        printLetters();
+    }
+
+    //printing Letters
+    public static void printLetters() {
         System.out.println("first");
         System.out.println("second");
         System.out.println("third");
