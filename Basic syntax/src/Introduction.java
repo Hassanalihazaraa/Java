c374ca136590dd856aa7dc29cddcb36e009b25f3
Hassan <hassanalihazaraa@gmail.com>
2020-10-02 10:23:20 +0200

    printing data

---
+++ Basic syntax/src/Introduction.java
@@ -0,0 +1,8 @@
+public class Introduction {
+    //printing data
+    public static void main(String[] args) {
+        System.out.println("first");
+        System.out.println("second");
+        System.out.println("third");
+    }
+}
