218f3e5285fb1692da58dd4ce90b3a20996b5835
Hassan <hassanalihazaraa@gmail.com>
2020-10-06 14:05:15 +0200

    stage 2

---
+++ simple chatty bot/src/bot/WhatsMyName.java
@@ -0,0 +1,16 @@
+package bot;
+
+import java.util.Scanner;
+
+public class WhatsMyName {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+        // reading a name
+        String myName = scanner.next();
+        System.out.println("What a great name you have, " + myName + " !");
+    }
+}
