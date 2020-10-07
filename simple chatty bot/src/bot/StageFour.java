edd7d88d65dd558a26b3b87c9d77c9f6601d035c
Hassan <hassanalihazaraa@gmail.com>
2020-10-07 15:45:38 +0200

    Check the sum

--- simple chatty bot/src/bot/StageFour.java
+++ simple chatty bot/src/bot/StageFour.java
@@ -4,7 +4,18 @@ import java.util.*;
 
 public class StageFour {
     public static void main(String[] args) {
-
+        checkSum();
     }
 
+    public static void checkSum() {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (a + b == 20 || b + c == 20 || a + c == 20) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
 }
